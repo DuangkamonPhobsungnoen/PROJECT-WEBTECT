db286b2294c7febf1a4f9ed0ee6347c12f6dee0e
Dnuangkamon <88492141+Dnuangkamon@users.noreply.github.com>
2022-10-19 18:16:25 +0700

    แก้ไข slides

--- Poster.php
+++ Poster.php
@@ -1,10 +1,11 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Home</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -12,41 +13,31 @@
 </head>
 
 <body>
-    <!--content -->
-    <div class="container">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="d-block w-100">
-                        <div class="featured-item row align-items-stretch">
-                            <img src="https://media.discordapp.net/attachments/1031228521613381724/1031971926349979658/2.png?width=1440&height=480" width="100%" height="100%">
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="d-block w-100">
-                        <div class="featured-item row align-items-stretch">
-                            <img src="https://media.discordapp.net/attachments/1031228521613381724/1031971926727471194/1.png?width=1440&height=480" width="100%" height="100%">
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="d-block w-100">
-                        <div class="featured-item row align-items-stretch">
-                            <img src="https://media.discordapp.net/attachments/1031228521613381724/1031971927109144606/3.png?width=1440&height=480" width="100%" height="100%">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class='container'>
+        <img class="Slides" src="https://media.discordapp.net/attachments/1031228521613381724/1031971926349979658/2.png?width=1440&height=480" width="100%">
+        <img class="Slides" src="https://media.discordapp.net/attachments/1031228521613381724/1031971926727471194/1.png?width=1440&height=480" width="100%">
+        <img class="Slides" src="https://media.discordapp.net/attachments/1031228521613381724/1031971927109144606/3.png?width=1440&height=480" width="100%">
     </div>
+
+
+    <script>
+        var slideIndex = 0;
+        carousel();
+
+        function carousel() {
+            var i;
+            var x = document.getElementsByClassName("Slides");
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > x.length) {
+                slideIndex = 1
+            }
+            x[slideIndex - 1].style.display = "block";
+            setTimeout(carousel, 2800);
+        }
+    </script>
 </body>
 
 </html>
-
-<!-- <?php include("Poster.html"); ?> <br> -->
