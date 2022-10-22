58b453e6d1368d9cfc571a2eecd8440813f5d4bf
Sutheera P <64070112@kmitl.ac.th>
2022-10-22 19:47:56 +0700

    book detail ยังไม่ได้
    ติดปัญหา function

--- script.js
+++ script.js
@@ -17,14 +17,33 @@ function showBook(category) {
         }   
 
 }
-
-var cover = "";
-var title = "";
-var writer = "";
-var price = 0;
-var cover = "";
 var id = 0;
+var cover = "";
+var title = "Title";
+var writer = "Writer";
+var price = 0;
+var descrip = "เรื่องย่อ";
 
-function bookDetail(cover, category, detail, title, price){
-    let show = document.getElementById("bookDetail")
+function setBook(id1, cover1, title1, writer1, price1, des1){
+    id = id1;
+    cover = cover1;
+    title = title1;
+    writer = writer1;
+    price = price1;
+    descrip = des1;
+    bookDetail();
+}
+
+function bookDetail(){
+    let show = document.getElementById("bookDetail");
+    let text = "<div class='col-lg-6 col-sm-12 align-self-center'><img src='"+cover+"' class='img-fluid'></div>";
+    text += "<div class='col-lg-6 col-sm-12 align-self-center py-5 pr-3'>";
+    text += "<div class='h3 font-weight-bold mb-0'>"+title+"</div>";
+    text += "<div class='text-muted'>"+writer+"</div>";
+    text += "<div class='h3 font-weight-bold my-4' style='color:var(--green);'>฿"+price+"</div>";
+    text += "<div class='p detail'>"+descrip+"</div>";
+    text += "<button class='btn-cart small mt-4 px-5 py-2' onclick='#'>ADD TO CART</button>";
+    text += "</div></div>";
+
+    show.innerHTML = text;
 }
