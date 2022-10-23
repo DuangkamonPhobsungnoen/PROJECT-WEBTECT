fb1aaad34c78c97d4826e5fce3d1ac63665c2ab7
Dnuangkamon <88492141+Dnuangkamon@users.noreply.github.com>
2022-10-23 11:23:10 +0700

    แก้ไข slideBook
    แก้ไขปุ่มทับตัวหนังสือ

--- slideBook.php
+++ slideBook.php
@@ -1,4 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</head>
+<body>
+    <?php
 
     $url = "book.json";
     $response = file_get_contents($url);
@@ -23,7 +37,7 @@
                                 <img src='https://$book->cover' width='100%' class='img-fluid rounded' /></a>
                             </div>
                             <div class='col-lg-7 my-auto'>
-                                <div class='info'
+                                <div class='info' style='padding: 5%'>
                                     <h4 class='entry-title'><a href='https://salmonbooks.net/book/maohai/'>$book->title</a></h4>
                                     <div class='entry-meta mb-4'>
                                         <span> $book->writer </span>
@@ -51,3 +65,5 @@
             </div>
         </div>";
     ?>
+</body>
+</html>
